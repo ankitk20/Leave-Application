835074b9a0978ced07a8a6eeee644e3c9fa1652a
abhinav96 <valecha.abhinav@gmail.com>
2016-09-05 00:35:08 +0530

    --

--- js/saveform.js
+++ js/saveform.js
@@ -1,36 +1,30 @@
 $(function(){
-
 	$("#apply").on("click",function(){
-
-			$applyTo = $("#applyTo").val();
-			$fromDate = $("#fromDate").val();
-			$toDate = $("#toDate").val();
-			$type = $("#typeOfLeave").val();
-			$note = $("#note").val();
-			
-			if( $applyTo && $fromDate && $type){
-
-				$.ajax({
-					url: "../php/saveform.php",
-					type:"POST",
-					data:{
-								appliedTo: $applyTo,
-							  fromDate: $fromDate,
-							  toDate: $toDate,
-							  type: $type,
-							  note: $note
-							},
-					success: function($result){
-										console.log($result);
-										if($result == -1)
-											window.location.href = "history.php";
-									},
-					error: function(){
-									console.log("error")
-						   	} 
-				});
-			}
-
+		$applyTo = $("#applyTo").val();
+		$fromDate = $("#fromDate").val();
+		$toDate = $("#toDate").val();
+		$type = $("#typeOfLeave").val();
+		$note = $("#note").val();
+		if( $applyTo && $fromDate && $type){
+			$.ajax({
+				url: "../php/saveform.php",
+				type:"POST",
+				data:{
+					appliedTo: $applyTo,
+					fromDate: $fromDate,
+					toDate: $toDate,
+					type: $type,
+					note: $note
+				},
+				success: function($result){
+					console.log($result);
+					if($result == -1)
+					window.location.href = "history.php";
+				},
+				error: function(){
+					console.log("error");
+				} 
+			});
+		}
 	});
-
 });
