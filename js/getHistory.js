fadd82367a2213d9cf953c51239c9254810588b2
abhinav96 <valecha.abhinav@gmail.com>
2016-09-05 00:34:30 +0530

    -- loading spinner

--- js/getHistory.js
+++ js/getHistory.js
@@ -1,20 +1,22 @@
 $(function(){
+	$('table').hide();
 	$.ajax({
 		url:"../php/getHistory.php",
 		datatype:"json",
 		type:"post",
 		success:function($row){
-			console.log($.parseJSON($row));
 			$.each($.parseJSON($row),function(column,value){
 				$("tbody").append("<tr></tr>");
 				$.each(value, function(col,val){
 					$("tbody>tr:eq(" + column + ")").append("<td>"+val+"</td>");
+					$("tbody>tr:eq(" + column + ")").addClass("mdl-data-table__cell--non-numeric");
 				});
 			});
+			$('.mdl-spinner').hide();
+			$('table').show();
 		},
 		error: function($result){
 			alert("he maa!! maataji.");
 		}
 	});
-
 });
