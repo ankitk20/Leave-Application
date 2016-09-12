e4d3a5b274dbe6f0772e0ac3451bb47215715b8e
Ankit Kesharwani <Ankit Kesharwani>
2016-09-12 15:03:41 +0530

    getapplication.js modified

--- js/getApplicant.js
+++ js/getApplicant.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 			$.each($.parseJSON($row),function(column,value){
 				if(value['ID'] == null){
 					console.log('EMPTY');
-					$("tbody").append('<tr><td style="text-align: center;" colspan="7">No leave applications to show.</td></tr>');
+					$("tbody").append('<tr><td style="text-align: center;" colspan="8">No leave applications to show.</td></tr>');
 				}
 				else{
 					$("tbody").append("<tr></tr>");
@@ -22,11 +22,23 @@ $(document).ready(function(){
 								$("tbody>tr:eq(" + column + ")").append("<td class='mdl-data-table__cell--non-numeric'>"+moment(val).format("ddd, MMM D YYYY")+"</td>");
 								break;
 							}
+							case 'Note':{
+								if(val!=null){
+									console.log(val+col);
+									$("tbody>tr:eq(" + column + ")").attr('value',val);
+								}
+								else{
+									console.log(val+col);
+									$("tbody>tr:eq(" + column + ")").attr('value',val);
+								}
+								break;
+							}
 							default:{
 								$("tbody>tr:eq(" + column + ")").append("<td class='mdl-data-table__cell--non-numeric'>"+val+"</td>");
 							}
 						}
 					});
+				
 					$("tbody>tr:eq(" + column + ")").append("<td class='mdl-data-table__cell--non-numeric'><button class='mdl-button mdl-js-button mdl-js-ripple-effect' value='accept'>Accept</button></td>");
 					$("tbody>tr:eq(" + column + ")").append("<td class='mdl-data-table__cell--non-numeric'><button class='mdl-button mdl-js-button mdl-js-ripple-effect' value='reject'>Reject</button></td>");
 				}
