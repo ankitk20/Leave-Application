724a85292c7b772dbaea4f71f0fb995fe9c24f91
Ankit Kesharwani <Ankit Kesharwani>
2016-09-17 22:38:09 +0530

    query file ki mkc

--- php/admin.php
+++ php/admin.php
@@ -2,15 +2,25 @@
 
 	session_start();
 	$connection = new mysqli("localhost","root","","Leave-Application");
-	$sickquery="SELECT Google_UID,`Sick Leave` FROM PendingLeave";
-	$query="UPDATE PendingLeave SET `Sick Leave`= ?, `Casual Leave`=?, `Vacation`=?, `Early Go`=?, `Earned Leave`=? WHERE Google_UID=?";
+	$sickquery="SELECT Google_UID,`Sick Leave`,`Earned Leave` FROM PendingLeave";
+
+	$query="UPDATE PendingLeave SET `Sick Leave`= ?, `Casual Leave`=?, `Vacation`=?, `Early Go`=?, `Earned Leave`=? WHERE `Google_UID`=?";
+
 
 	$sickstatement=$connection->prepare($sickquery);
 	$sickstatement->execute();
-	$statement=$connection->prepare($query);
-	//store the rows in an assoc array and then bind data in loop
-	for ($i=0; $i</*sickresult.length()*/; $i++) {  
-		$statement->bind_param("iiiiis",5+$result['Sick Leave'],4,0,4,);
+	$sickstatement->bind_result($Google_UID,$SickLeave,$EarnedLeave);
+	
+
+	if($statement=$connection->prepare($query)){
+		
+	}else{
+		print_r("failed to prepare");
+		$connection->errno;
+	}
+	while ($sickstatement->fetch()) {  
+
+		$statement->bind_param("iiiiis",5+$SickLeave,4,0,4,$EarnedLeave,$Google_UID);
 		$statement->execute();
 	}
 	
